7643d6af4314db5121d8d16953422888a2de3321
재용 성 <mavi1969@naver.com>
2026-06-12 20:40:51 +0900

    암호화 패턴 수정

--- src/java/com/PatternEnum.java
+++ src/java/com/PatternEnum.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public enum PatternEnum {
 	NODEF("^(?<allData>.*)$"),
-	ENCRYPT("(?<encData>\\$\\.[^=]+={1,})"),
+	ENCRYPT("(?<encData>\\$\\.[^=]+={0,})"),
 	EMAIL("^(?<firstName>[A-Za-z0-9._%+-]+)@(?<lastName>[A-Za-z0-9.-]+\\.[A-Za-z]{2,})$"),
 	PHONE_NUMBER("^(?<firstNumber>\\d{2,3})([.-])(?<secondNumber>\\d{3,4})\\2(?<lastNumber>\\d{4})$");
 
